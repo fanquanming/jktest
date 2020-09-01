5abe0fb1ee17696ea499a88455027efc6f74aeac
fanquanming <2836564875@qq.com>
2020-09-01 15:45:35 +0800

    jenkinsspringboot项目测试

---
+++ src/test/java/com/it/jktest/JktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.jktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
